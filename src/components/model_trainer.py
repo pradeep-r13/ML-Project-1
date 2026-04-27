573c627dbd41a91c1ecdf79f9b42005be7fff27b
Prdp41 <officialpradeep13@gmail.com>
2026-04-27 18:31:53 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,12 +44,76 @@ class Modeltrainer:
                 'CatBoostRegressor': CatBoostRegressor(verbose=False)
             }
 
-            model_report: dict = evaluate_models(
-                x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models)
+            
+
+            params = {
+
+                "Random Forest Regressor": {
+                    'n_estimators': [100, 200],
+                    'max_depth': [None, 10, 20],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2]
+                },
+
+                "AdaBoost Regressor": {
+                    'n_estimators': [50, 100, 200],
+                    'learning_rate': [0.01, 0.1, 0.5]
+                },
+
+                "GradientBoosting Regressor": {
+                    'n_estimators': [100, 200],
+                    'learning_rate': [0.01, 0.1],
+                    'max_depth': [3, 5],
+                    'subsample': [0.8, 1.0]
+                },
+
+                "Linear Regression": {
+                    # no hyperparameters
+                },
+
+                "SVR": {
+                    'kernel': ['rbf', 'linear'],
+                    'C': [1, 10],
+                    'epsilon': [0.1, 0.2],
+                    'gamma': ['scale', 'auto']
+                },
+
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [3, 5, 7],
+                    'weights': ['uniform', 'distance'],
+                    'metric': ['euclidean', 'manhattan']
+                },
+
+                "DecisionTreeRegressor": {
+                    'max_depth': [None, 10, 20],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2]
+                },
+
+                "XGBRegressor": {
+                    'n_estimators': [100, 200],
+                    'learning_rate': [0.01, 0.1],
+                    'max_depth': [3, 5],
+                    'subsample': [0.8, 1.0],
+                    'colsample_bytree': [0.8, 1.0]
+                },
+
+                "CatBoostRegressor": {
+                    'iterations': [100, 200],
+                    'learning_rate': [0.01, 0.1],
+                    'depth': [6, 8],
+                    'l2_leaf_reg': [1, 3]
+                }
+            }
+
+            
+
+            best_estimater, model_report = evaluate_models(
+                x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models, param=params)
 
             # To get best model score from dict
 
-            best_model_score = max(sorted(model_report.values()))
+            best_model_score = max(model_report.values())
 
             # To get best model name from dict
 
@@ -57,7 +121,7 @@ class Modeltrainer:
                 model_report.values()).index(best_model_score)]
 
             best_model = models[best_model_name]
-
+            best_estimater = best_estimater[best_model_name]
             if best_model_score < 0.6:
                 raise CustomException('No Best Model Found', sys)
 
@@ -66,11 +130,11 @@ class Modeltrainer:
 
             save_object(
                 file_path=self.model_trainer_config.train_model_file_path, obj=best_model)
-
-            Predicted = best_model.predict(x_test)
+            logging.info('save_object')
+            Predicted = best_estimater.predict(x_test)
 
             r2_square = r2_score(y_test, Predicted)
             return r2_square
 
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e, sys)
